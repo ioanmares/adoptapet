55b3dacde32de5eff5e7e0d29d6370bac0ea4dfd
Ioan Mares <ioan.mares@mhcautomation.com>
2021-11-15 13:24:25 +0200

    Small improvement.

--- src/pages/login/LoginForm.vue
+++ src/pages/login/LoginForm.vue
@@ -1,15 +1,7 @@
 <template>
   <div class="mx-auto mt-4 w-full max-w-xs">
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-8">
-        <!-- <Select
-          :ref="setItemRef"
-          name="category"
-          label="Category"
-          :items="[{ text: 'Gigel', value: 'Are mere' }]"
-          required
-          v-model="state.category"
-        /> -->
+      <div class="mb-4">
         <Input
           :ref="setItemRef"
           name="username"
