b6d376f94f5ddb53e8c6da261703a5f9d97e19da
Ioan Mares <ioan.mares@mhcautomation.com>
2021-11-15 16:04:36 +0200

    Update mocks.

--- src/services/pets.mock.js
+++ src/services/pets.mock.js
@@ -6,7 +6,7 @@ export const pets = [
     name: "Max",
     category: "dog",
     description:
-      "Max is a 2 years old bulldog. He is very playful and knows a trick or two when it comes to eating your pizza.",
+      "Max is a 2 years old pomeranian. He is very playful and knows a trick or two when it comes to eating your pizza.",
     photos: MaxPhotos,
     userId: "9ecd874c-d410-4917-ad68-1acd1f0bbe90",
   },
@@ -19,39 +19,11 @@ export const pets = [
     photos: [],
   },
   {
-    id: "3",
-    name: "Max",
+    id: "2",
+    name: "Fred",
     category: "dog",
     description:
-      "Max is a 2 years old bulldog. He is very playful and knows a trick or two when it comes to eating your pizza.",
-    photos: MaxPhotos,
-    userId: "9ecd874c-d410-4917-ad68-1acd1f0bbe90",
-  },
-  {
-    id: "4",
-    name: "Max",
-    category: "dog",
-    description:
-      "Max is a 2 years old bulldog. He is very playful and knows a trick or two when it comes to eating your pizza.",
-    photos: MaxPhotos,
-    userId: "9ecd874c-d410-4917-ad68-1acd1f0bbe90",
-  },
-  {
-    id: "5",
-    name: "Max",
-    category: "dog",
-    description:
-      "Max is a 2 years old bulldog. He is very playful and knows a trick or two when it comes to eating your pizza.",
-    photos: MaxPhotos,
-    userId: "9ecd874c-d410-4917-ad68-1acd1f0bbe90",
-  },
-  {
-    id: "6",
-    name: "Max",
-    category: "dog",
-    description:
-      "Max is a 2 years old bulldog. He is very playful and knows a trick or two when it comes to eating your pizza.",
-    photos: MaxPhotos,
-    userId: "9ecd874c-d410-4917-ad68-1acd1f0bbe90",
+      "Fred is a 2 years old bulldog. He enjoys surprising people when they least expect it. It is definitely a pick if you want a big laugh.",
+    photos: [],
   },
 ];
